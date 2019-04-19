42e31e54f2e8ce819677bb4b7b4e329112a8b64a
Tim <5472896+Zukunftsmusik@users.noreply.github.com>
2019-04-19 13:50:42 +0200

    Added function 'get_user_pss_toolkit'

--- src/utility.py
+++ src/utility.py
@@ -1,6 +1,14 @@
+import discord
 import subprocess
+
+user_pss_toolkit = None
 
 def shell_cmd(cmd):
     result = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
     return result.stdout.decode('utf-8')
+    
+def get_user_pss_toolkit():
+    if user_pss_toolkit == None:
+        user_pss_toolkit = client.get_user('487398795756437514')
+    return user_pss_toolkit
     
