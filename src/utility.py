f196358f14b2d4553f71021759debd2cf2288a2a
Tim <5472896+Zukunftsmusik@users.noreply.github.com>
2019-04-19 14:04:48 +0200

    Update utility.py

--- src/utility.py
+++ src/utility.py
@@ -1,14 +1,10 @@
 import discord
 import subprocess
 
-user_pss_toolkit = None
-
 def shell_cmd(cmd):
     result = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
     return result.stdout.decode('utf-8')
     
 def get_user_pss_toolkit():
-    if user_pss_toolkit == None:
-        user_pss_toolkit = discord.client.get_user('487398795756437514')
-    return user_pss_toolkit
+    return discord.client.get_user('487398795756437514')
     
