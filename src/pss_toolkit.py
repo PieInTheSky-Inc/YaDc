f348fcdb0be6e663584af21829921393e5a4df80
Tim <5472896+Zukunftsmusik@users.noreply.github.com>
2019-04-19 12:15:07 +0200

    Update pss_toolkit.py

--- src/pss_toolkit.py
+++ src/pss_toolkit.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# ----- Packages ------------------------------------------------------
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import argparse
+import datetime
+import pss_core as core
+import pss_prestige as p
+import pss_research as rs
+import xml.etree.ElementTree
+
+
+base_url = 'http://{}/'.format(core.get_production_server())
 
