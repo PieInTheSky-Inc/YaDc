dad2699004d5419d696208c624ec0e3dc954507a
Tim <5472896+Zukunftsmusik@users.noreply.github.com>
2019-04-19 14:54:20 +0200

    Update utility.py

--- src/utility.py
+++ src/utility.py
@@ -1,10 +1,5 @@
-import bot
-import discord
 import subprocess
 
 def shell_cmd(cmd):
     result = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
     return result.stdout.decode('utf-8')
-    
-#def get_user_pss_toolkit():
-#    return bot.get_user(487398795756437514)
