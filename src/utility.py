e542e311f39338a947dfb4a70b7b6ea637b5fcfe
Tim <5472896+Zukunftsmusik@users.noreply.github.com>
2019-04-19 14:39:02 +0200

    Update utility.py

--- src/utility.py
+++ src/utility.py
@@ -6,5 +6,5 @@ def shell_cmd(cmd):
     result = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
     return result.stdout.decode('utf-8')
     
-def get_user_pss_toolkit():
-    return bot.get_user(487398795756437514)
+#def get_user_pss_toolkit():
+#    return bot.get_user(487398795756437514)
