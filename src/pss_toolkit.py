c9b90d87e90ec68c90d913e3438f7dc9c4c936e3
Tim <5472896+Zukunftsmusik@users.noreply.github.com>
2019-04-19 12:37:40 +0200

    Commented whole file

--- src/pss_toolkit.py
+++ src/pss_toolkit.py
@@ -5,18 +5,18 @@
 
 
 # ----- Packages ------------------------------------------------------
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+#from __future__ import absolute_import
+#from __future__ import division
+#from __future__ import print_function
+#from __future__ import unicode_literals
 
-import argparse
-import datetime
-import pss_core as core
+#import argparse
+#import datetime
+#import pss_core as core
 
 
-base_url = 'http://{}/'.format(core.get_production_server())
+#base_url = 'http://{}/'.format(core.get_production_server())
 
-async def get_fleet_spreadsheet(ctx, fleet_name)
-    txt = '#fleet ' + fleet_name
-    await ctx.send(txt)
+#async def get_fleet_spreadsheet(ctx, fleet_name)
+#    txt = '#fleet ' + fleet_name
+#    await ctx.send(txt)
