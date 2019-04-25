3b91e588b2f5f5b065d8f73d42d5885d62949ee3
Tim <5472896+Zukunftsmusik@users.noreply.github.com>
2019-04-25 23:47:27 +0200

    Added datetime functions
    get_first_of_following_month(datetime): datetime
    get_first_of_next_month(): datetime
    get_formatted_datetime(datetime): str
    get_utcnow()

--- src/utility.py
+++ src/utility.py
@@ -3,3 +3,27 @@ import subprocess
 def shell_cmd(cmd):
     result = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
     return result.stdout.decode('utf-8')
+
+
+def get_first_of_following_month(utcnow):
+    year = utcnow.year
+    month = utcnow.month + 1
+    if (month == 13):
+        year += 1
+        month = 1
+    next_first_of_month = datetime(year, month, 1, 0, 0, 0, 0, timezone.utc)
+    return next_first_of_month
+    
+
+def get_first_of_next_month():
+    utcnow = get_utcnow()
+    return get_first_of_following_month(utcnow)
+
+
+def get_formatted_datetime(date_time):
+    txt = date_time.strftime('%Y-%m-%d %H:%M:%S')
+    return txt
+
+
+def get_utcnow():
+    return datetime.now(timezone.utc)
