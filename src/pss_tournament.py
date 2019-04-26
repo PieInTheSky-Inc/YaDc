f39f5e8530ffea2861f9e4d7e9da40312a067550
Tim <5472896+Zukunftsmusik@users.noreply.github.com>
2019-04-26 12:00:02 +0200

    Created tourney start functions
    get_current_tourney_start(): datetime
    - returns the start datetime of the tournament in the current month
    
    get_next_tourney_start(): datetime
    - returns the start datetime of the tournament in the next month

--- src/pss_tournament.py
+++ src/pss_tournament.py
@@ -8,18 +8,20 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from datetime import date, datetime, time, timedelta, timezone
 
-import argparse
-import datetime
-import os
-import pandas as pd
-import re
-import urllib.request
-import uuid
-import xml.etree.ElementTree
-
 import pss_core as core
+import utility
 
 
 base_url = 'http://{}/'.format(core.get_production_server())
+a_week_prior = timedelta(-7)
 
 # ----- Utility methods ---------------------------------------------------------
+def get_current_tourney_start():
+    first_of_next_month = utility.get_first_of_next_month()
+    result = first_of_next_month + a_week_prior
+    return result
+
+def get_next_tourney_start():
+    next_first_of_next_month = utility.get_first_of_following_month(utility.get_first_of_next_month())
+    result = next_first_of_next_month + a_week_prior
+    return result
