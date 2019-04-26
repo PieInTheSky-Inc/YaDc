680b47e788203fb08181d43572e8740f649e4129
Tim <5472896+Zukunftsmusik@users.noreply.github.com>
2019-04-26 13:04:34 +0200

    Added remaining tourney time

--- src/pss_tournament.py
+++ src/pss_tournament.py
@@ -16,6 +16,27 @@ base_url = 'http://{}/'.format(core.get_production_server())
 a_week_prior = timedelta(-7)
 
 # ----- Utility methods ---------------------------------------------------------
+def format_tourney_start(start_date, utcnow):
+    print('format_tourney_start({}, {})'.format(start_date, utcnow))
+    currently_running = is_tourney_running(start_date, utcnow)
+    starts = get_start_string(currently_running)
+    print('created string: starts = \'starts\'')
+    formatted_date = utility.get_formatted_datetime(start_date)
+    print('formatted specified datetime object')
+    result = 'Tournament in {} {} on: {}\n'.format(start_date.strftime('%B'), starts, formatted_date)
+    print('created output string: {}'.format(result))
+    delta_start = start_date - utcnow
+    print('calculated delta of start_date & utcnow: {}'.format(delta_start))
+    if currently_running:
+        end_date = utility.get_first_of_following_month(start_date)
+        delta_end = end_date - utcnow
+        result += '({}, running for another {})'.format(utility.get_formatted_timedelta(delta_start), utility.get_formatted_timedelta(delta_end))
+    else:
+        result += '({})'.format(utility.get_formatted_timedelta(delta_start))
+    print('created result: {}'.format(result))
+    return result
+
+
 def get_current_tourney_start():
     first_of_next_month = utility.get_first_of_next_month()
     result = first_of_next_month + a_week_prior
@@ -28,19 +49,12 @@ def get_next_tourney_start():
     return result
 
 
-def format_tourney_start(start_date, utcnow):
-    print('format_tourney_start({}, {})'.format(start_date, utcnow))
-    starts = 'starts'
-    print('created string: starts = \'starts\'')
-    if start_date < utcnow:
-        starts = 'started'
-        print('set starts = \'started\'')
-    formatted_date = utility.get_formatted_datetime(start_date)
-    print('formatted specified datetime object')
-    result = 'Tournament in {} {} on: {}\n'.format(start_date.strftime('%B'), starts, formatted_date)
-    print('created output string: {}'.format(result))
-    delta = start_date - utcnow
-    print('calculated delta of start_date & utcnow: {}'.format(delta))
-    result += '({})'.format(utility.get_formatted_timedelta(delta))
-    print('created result: {}'.format(result))
-    return result
+def get_start_string(currently_running):
+    if currently_running:
+        return 'started'
+    else:
+        return 'starts'
+
+
+def is_tourney_running(start_date, utcnow):
+    return start_date < utcnow
