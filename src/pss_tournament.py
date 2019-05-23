44d707a6593e845906039335733a2d7c6a4e1e57
Tim <5472896+Zukunftsmusik@users.noreply.github.com>
2019-05-23 13:13:05 +0200

    Fixed

--- src/pss_tournament.py
+++ src/pss_tournament.py
@@ -8,6 +8,8 @@ import pss_core as core
 import utility as util
 
 
+A_WEEK_PRIOR = timedelta(-7)
+
 # ----- Tournament methods ---------------------------------------------------------
 def format_tourney_start(start_date, utc_now):
     currently_running = is_tourney_running(start_date, utc_now)
@@ -23,7 +25,7 @@ def format_tourney_start(start_date, utc_now):
     if currently_running:
         end_date = utility.get_first_of_following_month(start_date)
         end_date_formatted = util.get_formatted_date(end_date)
-        delta_end = end_date - utcnow
+        delta_end = end_date - utc_now
         delta_end_formatted = util.get_formatted_timedelta(delta_end, False)
         delta_end_txt = ' and goes on for another **{}** (until {})'.format(delta_end_formatted, end_date_formatted)
     result = 'Tournament in {} {} {}{}'.format(tourney_month, starts, delta_start_txt, delta_end_txt)
@@ -32,13 +34,13 @@ def format_tourney_start(start_date, utc_now):
 
 def get_current_tourney_start():
     first_of_next_month = util.get_first_of_next_month()
-    result = first_of_next_month + a_week_prior
+    result = first_of_next_month + A_WEEK_PRIOR
     return result
 
 
 def get_next_tourney_start():
     next_first_of_next_month = util.get_first_of_following_month(util.get_first_of_next_month())
-    result = next_first_of_next_month + a_week_prior
+    result = next_first_of_next_month + A_WEEK_PRIOR
     return result
 
 
@@ -49,5 +51,5 @@ def get_start_string(currently_running):
         return 'starts'
 
 
-def is_tourney_running(start_date, utcnow):
-    return start_date < utcnow
+def is_tourney_running(start_date, utc_now):
+    return start_date < utc_now
