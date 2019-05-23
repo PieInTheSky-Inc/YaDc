50b021ccef7fdff27ac605391b55e741bf295920
Tim <5472896+Zukunftsmusik@users.noreply.github.com>
2019-05-23 12:32:56 +0200

    Changed output of format_tourney_start

--- src/pss_tournament.py
+++ src/pss_tournament.py
@@ -15,7 +15,8 @@ def format_tourney_start(start_date, utc_now):
     start_date_formatted = util.get_formatted_date(start_date)
     tourney_month = start_date.strftime('%B')
     delta_start = start_date - utc_now
-    delta_start_formatted = '{} ({})'.format(util.get_formatted_timedelta(delta_start), start_date_formatted)
+    delta_start_formatted = util.get_formatted_timedelta(delta_start)
+    delta_start_txt = '**{}** ({})'.format(delta_start_formatted, start_date_formatted)
     currently_running_txt = ''
     delta_end_formatted = ''
     result = 'Tournament in {} {} '.format(start_date.strftime('%B'), starts)
@@ -24,8 +25,8 @@ def format_tourney_start(start_date, utc_now):
         end_date_formatted = util.get_formatted_date(end_date)
         delta_end = end_date - utcnow
         delta_end_formatted = util.get_formatted_timedelta(delta_end, False)
-        currently_running_txt = ' and goes on for another **{}** (until {})'.format(delta_end_formatted, end_date_formatted)
-    result = 'Tournament in {} {} **{}**{}'.format(tourney_month, starts, delta_start_formatted, currently_running_txt)
+        delta_end_txt = ' and goes on for another **{}** (until {})'.format(delta_end_formatted, end_date_formatted)
+    result = 'Tournament in {} {} {}{}'.format(tourney_month, starts, delta_start_txt, delta_end_txt)
     return result
 
 
