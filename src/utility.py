670ecfbcf4aa4d27293f0932449bec8a90d200cb
Tim <5472896+Zukunftsmusik@users.noreply.github.com>
2019-05-23 11:30:54 +0200

    Merged from beta branch

--- src/utility.py
+++ src/utility.py
@@ -1,5 +1,6 @@
 from datetime import date, datetime, time, timedelta, timezone
 
+import discord
 import math
 import subprocess
 
@@ -62,3 +63,107 @@ def get_formatted_timedelta(delta, include_relative_indicator=True):
 
 def get_utcnow():
     return datetime.now(timezone.utc)
+
+
+async def get_latest_message(from_channel, by_member_id=None, with_content=None, after=None, before=None):
+    if from_channel is not None:
+        messages = from_channel.history(limit=100, after=after, before=before, older_first=True).flatten()
+        for msg in reversed(messages):
+            process = not by_member_id or msg.author.id == by_member_id
+            if process and msg.content == with_content:
+                return msg
+    return None      
+
+
+
+#---------- DB utilities ----------
+DB_TIMESTAMP_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+def db_get_column_definition(column_name, column_type, is_primary=False, not_null=False):
+    column_name_txt = column_name.upper()
+    column_type_txt = column_type.upper()
+    is_primary_txt = ''
+    not_null_txt = ''
+    if is_primary:
+        is_primary_txt = ' PRIMARY KEY'
+    if not_null:
+        not_null_txt = ' NOT NULL'
+    result = '{} {}{}{}'.format(column_name_txt, column_type_txt, is_primary_txt, not_null_txt)
+    return result
+
+
+def db_get_where_and_string(where_strings):
+    if where_strings:
+        result = ''
+        for i in range(0, len(where_strings)):
+            if i > 0:
+                result += ' AND '
+            result += where_strings[i]
+        return result
+
+
+def db_get_where_or_string(where_strings):
+    if where_strings:
+        result = ''
+        for i in range(0, len(where_strings)):
+            if i > 0:
+                result += ' OR '
+            result += where_strings[i]
+        return result
+
+
+def db_get_where_string(column_name, column_value, is_text_type=False):
+    column_name = column_name.lower()
+    if is_text_type:
+        column_value = db_convert_text(column_value)
+    return '{} = {}'.format(column_name, column_value)
+
+
+def db_convert_boolean(value):
+    if value:
+        return 'TRUE'
+    else:
+        return 'FALSE'
+    
+def db_convert_text(value):
+    if value:
+        result = str(value)
+        result = result.replace('\'', '\'\'')
+        result = '\'{}\''.format(result)
+        return result
+    else:
+        return ''
+    
+def db_convert_timestamp(datetime):
+    if datetime:
+        result = 'TIMESTAMPTZ \'{}\''.format(datetime.strftime(DB_TIMESTAMP_FORMAT))
+        return result
+    else:
+        return None
+
+def db_convert_to_boolean(db_boolean):
+    if db_boolean is None:
+        return None
+    db_upper = db_boolean.upper()
+    if db_upper == 'TRUE' or db_upper == '1' or db_upper == 'T' or db_upper == 'Y' or db_upper == 'YES':
+        return True
+    else:
+        return False
+    
+def db_convert_to_datetime(db_timestamp):
+    if db_timestamp is None:
+        return None
+    result = db_timestamp.strptime(DB_TIMESTAMP_FORMAT)
+    return result
+
+def db_convert_to_int(db_int):
+    if db_int is None:
+        return None
+    result = int(db_int)
+    return result
+
+def db_convert_to_float(db_float):
+    if db_float is None:
+        return None
+    result = float(db_float)
+    return result
