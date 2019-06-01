c9ff2055bb4c3ae6907875f0e8c365aeeb8355e6
Tim <5472896+Zukunftsmusik@users.noreply.github.com>
2019-06-01 16:25:32 +0200

    Fixed /test embed

--- src/bot.py
+++ src/bot.py
@@ -742,7 +742,7 @@ async def test(ctx, action, *, params=None):
         elif txt.startswith('[') and txt.endswith(']'):
             fiel = ast.literal_eval(txt)
         else:
-            fiel = [['Field Header'],txt]
+            fiel = [['Field Header',txt]]
         print(f'[test] retrieved fields: {fiel}')
         embe = util.create_embed(titl, desc, bot_colour, fiel)
         print(f'[test] created embed: {embe}')
