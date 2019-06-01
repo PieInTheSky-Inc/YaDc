50e74b588f2d837c71609725a3d6eead75b5bb97
Tim <5472896+Zukunftsmusik@users.noreply.github.com>
2019-06-01 15:00:09 +0200

    Edited get_command

--- src/utility.py
+++ src/utility.py
@@ -88,11 +88,11 @@ async def get_latest_message(from_channel, by_member_id=None, with_content=None,
 
 
 def get_command(commands_set, command_name):
-    command_name_parts = command_name.split('.')
+    command_name_parts = command_name.split(' ')
     for cmd in commands_set:
         if cmd.name == command_name_parts[0]:
             if len(command_name_parts) > 1:
-                subcommand_name = '.'.join(command_name_parts[1:])
+                subcommand_name = ' '.join(command_name_parts[1:])
                 return get_command(cmd.commands, subcommand_name)
             else:
                 return cmd
