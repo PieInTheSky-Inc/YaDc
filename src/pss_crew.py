2ccbd12af2db2489bb786c89957930d9ea1a5e4c
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2019-08-24 15:51:47 +0200

    Refactoring

--- src/pss_crew.py
+++ src/pss_crew.py
@@ -3,6 +3,7 @@
 
 from cache import PssCache
 import pss_core as core
+from pss_lookups import *
 import utility as util
 
 
@@ -18,40 +19,6 @@ COLLECTION_DESIGN_DESCRIPTION_PROPERTY_NAME = 'CollectionName'
 
 __PRESTIGE_FROM_BASE_PATH = f'CharacterService/PrestigeCharacterFrom?languagekey=en&characterDesignId='
 __PRESTIGE_TO_BASE_PATH = f'CharacterService/PrestigeCharacterTo?languagekey=en&characterDesignId='
-
-
-
-# ---------- Lookups ----------
-
-COLLECTION_PERK_LOOKUP = {
-    'BloodThirstSkill': 'Vampirism',
-    'EmpSkill': 'EMP Discharge',
-    'FreezeAttackSkill': 'Cryo Field',
-    'InstantKillSkill': 'Headshot',
-    'MedicalSkill': 'Combat Medic',
-    'ResurrectSkill': 'Resurrection',
-    'SharpShooterSkill': 'Sharpshooter'
-}
-
-EQUIPMENT_MASK_LOOKUP = {
-    1: 'head',
-    2: 'body',
-    4: 'leg',
-    8: 'weapon',
-    16: 'accessory'}
-
-SPECIAL_ABILITIES_LOOKUP = {
-    'AddReload': 'Rush Command',
-    'DamageToCurrentEnemy': 'Critical Strike',
-    'DamageToRoom': 'Ultra Dismantle',
-    'DamageToSameRoomCharacters': 'Poison Gas',
-    'DeductReload': 'System Hack',
-    'FireWalk': 'Fire Walk',
-    'Freeze': 'Freeze',
-    'HealRoomHp': 'Urgent Repair',
-    'HealSameRoomCharacters': 'Healing Rain',
-    'HealSelfHp': 'First Aid',
-    'SetFire': 'Arson'}
 
 
 
