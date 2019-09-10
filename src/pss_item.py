659a98579fc9bc710d87a6ba9e571d424bd52d01
Tim <5472896+Zukunftsmusik@users.noreply.github.com>
2019-09-10 22:45:30 +0200

    Fixed syntax typo

--- src/pss_item.py
+++ src/pss_item.py
@@ -1,275 +1,235 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+from datetime import date, datetime, time, timedelta, timezone
 
-import re
-
-from cache import PssCache
-import pss_core as core
-import utility as util
+import discord
+import math
+import subprocess
 
 
-# ---------- Constants ----------
-
-ITEM_DESIGN_BASE_PATH = 'ItemService/ListItemDesigns2?languageKey=en'
-ITEM_DESIGN_KEY_NAME = 'ItemDesignId'
-ITEM_DESIGN_DESCRIPTION_PROPERTY_NAME = 'ItemDesignName'
+def shell_cmd(cmd):
+    result = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
+    return result.stdout.decode('utf-8')
 
 
-
-# ---------- Initilization ----------
-
-__item_designs_cache = PssCache(
-    ITEM_DESIGN_BASE_PATH,
-    'ItemDesigns',
-    ITEM_DESIGN_KEY_NAME)
-
-
-
-# ---------- Item info ----------
-
-def get_item_details(item_name, as_embed=False):
-    item_infos = _get_item_infos(item_name)
-
-    if not item_infos:
-        return f'Could not find an item named **{item_name}**.', False
-    else:
-        if as_embed:
-            return _get_item_info_as_embed(item_infos), True
-        else:
-            return _get_item_info_as_text(item_infos), True
-
-
-def _get_item_infos(item_name, item_design_data=None, return_on_first=False):
-    if item_design_data is None:
-        item_design_data = __item_designs_cache.get_data_dict3()
-
-    item_design_ids = _get_item_design_ids_from_name(item_name, item_data=item_design_data, return_on_first=return_on_first)
-    result = [item_design_data[item_design_id] for item_design_id in item_design_ids if item_design_id in item_design_data.keys()]
-
+def get_first_of_following_month(utcnow):
+    year = utcnow.year
+    month = utcnow.month + 1
+    if (month == 13):
+        year += 1
+        month = 1
+    result = datetime(year, month, 1, 0, 0, 0, 0, timezone.utc)
     return result
 
 
-def _get_item_design_ids_from_name(item_name, item_data=None, return_on_first=False):
-    if item_data is None:
-        item_data = __item_designs_cache.get_data_dict3()
-
-    results = core.get_ids_from_property_value(item_data, ITEM_DESIGN_DESCRIPTION_PROPERTY_NAME, item_name, fix_data_delegate=_fix_item_name, return_on_first=return_on_first)
-    return results
+def get_first_of_next_month():
+    utcnow = get_utcnow()
+    return get_first_of_following_month(utcnow)
 
 
-def _get_item_info_as_embed(item_info):
-    return ''
-
-
-def _get_item_info_as_text(item_infos):
-    lines = ['**Item stats**']
-
-    for item_info in item_infos:
-        bonus_type = item_info['EnhancementType'] # if not 'None' then print bonus_value
-        bonus_value = item_info['EnhancementValue']
-        equipment_slot = item_info['ItemSubType']
-        item_name = item_info[ITEM_DESIGN_DESCRIPTION_PROPERTY_NAME]
-        item_type = item_info['ItemType']  # if 'Equipment' then print equipment slot
-        rarity = item_info['Rarity']
-
-        if item_type == 'Equipment' and 'Equipment' in equipment_slot:
-            slot_txt = ' ({})'.format(equipment_slot.replace('Equipment', ''))
+def get_formatted_datetime(date_time, include_tz=True, include_tz_brackets=True):
+    result = date_time.strftime('%Y-%m-%d %H:%M:%S')
+    if include_tz:
+        tz = date_time.strftime('%Z')
+        if include_tz_brackets:
+            result += ' ({})'.format(tz)
         else:
-            slot_txt = ''
-
-        if bonus_type == 'None':
-            bonus_txt = bonus_type
-        else:
-            bonus_txt = f'{bonus_type} {bonus_value}'
-
-        lines.append(f'{item_name} ({rarity}) - {bonus_txt}{slot_txt}')
-
-    return '\n'.join(lines)
-
-
-def _fix_item_name(item_name):
-    result = item_name.lower()
-    result = re.sub('[^a-z0-9]', '', result)
-    result = re.sub("(darkmatterrifle|dmr)(mark|mk)?(ii|2)", "dmrmarkii", result)
-    result = result.replace('anonmask', 'anonymousmask')
-    result = result.replace('armour', 'armor')
-    result = result.replace('bunny', 'rabbit')
-    result = result.replace('golden', 'gold')
+            result += ' {}'.format(tz)
     return result
 
 
-
-
-
-# ---------- Price info ----------
-
-def get_item_price(item_name: str, as_embed: bool = False):
-    item_infos = _get_item_infos(item_name)
-
-    if not item_infos:
-        return f'Could not find an item named **{item_name}**.', False
-    else:
-        if as_embed:
-            return _get_item_price_as_embed(item_name, item_infos), True
+def get_formatted_date(date_time, include_tz=True, include_tz_brackets=True):
+    result = date_time.strftime('%Y-%m-%d')
+    if include_tz:
+        tz = date_time.strftime('%Z')
+        if include_tz_brackets:
+            result += ' ({})'.format(tz)
         else:
-            return _get_item_price_as_text(item_name, item_infos), True
+            result += ' {}'.format(tz)
+    return result
 
 
-def _get_item_price_as_embed(item_name, item_infos):
-    return ''
-
-
-def _get_item_price_as_text(item_name, item_infos) -> str:
-    lines = []
-    lines.append(f'__**Item prices matching \'{item_name}\'**__')
-    lines.append('')
-    for item_info in item_infos:
-        flags = int(item_info['Flags'])
-        item_name = item_info[ITEM_DESIGN_DESCRIPTION_PROPERTY_NAME]
-        fair_price = item_info['FairPrice']
-        market_price = item_info['MarketPrice']
-        rarity = item_info['Rarity']
-
-        prices = f'{market_price} ({fair_price})'
-        if flags & 1 == 0:
-            prices = 'This item cannot be sold'
-
-        lines.append(f'{item_name} ({rarity}) - {prices}')
-
-    lines.append('')
-    lines.append('**Note:** 1st price is the market price. 2nd price is Savy\'s fair price. Market prices listed here may not always be accurate due to transfers between alts/friends or other reasons.')
-
-    return '\n'.join(lines)
-
-
-
-
-
-# ---------- Ingredients info ----------
-
-def get_ingredients_for_item(item_name: str, as_embed: bool = False):
-    util.dbg_prnt(f'+ get_ingredients_for_item({item_name}, {as_embed})')
-    if not item_name:
-        util.dbg_prnt(f'[get_ingredients_for_item] parameter \'item_name\' is either empty or None.')
-        return [f'You must specify an item name!'], False
-        
-    item_design_data = __item_designs_cache.get_data_dict3()
-    util.dbg_prnt(f'[get_ingredients_for_item] Retrieved item_design_data')
-    item_infos = _get_item_infos(item_name, item_design_data, return_on_first=True)
-    util.dbg_prnt(f'[get_ingredients_for_item] Retrieved item infos: {len(item_infos)} entries')
-
-    if not item_infos:
-        return [f'Could not find an item named **{item_name}**.'], False
-    else:
-        item_info = item_infos[0]
-        item_name = item_info[ITEM_DESIGN_DESCRIPTION_PROPERTY_NAME]
-        util.dbg_prnt(f'[get_ingredients_for_item] Retrieved item name: {item_name}')
-        ingredients_tree = _parse_ingredients_tree(item_info['Ingredients'], item_design_data)
-        util.dbg_prnt(f'[get_ingredients_for_item] Created ingredients tree')
-        ingredients_dicts = _flatten_ingredients_tree(ingredients_tree)
-        util.dbg_prnt(f'[get_ingredients_for_item] Parsed ingredients tree to list of dicts')
-        if as_embed:
-            return _get_item_ingredients_as_embed(item_name, ingredients_dicts, item_design_data), True
+def get_formatted_timedelta(delta, include_relative_indicator=True):
+    total_seconds = delta.total_seconds()
+    is_past = total_seconds < 0
+    if is_past:
+        total_seconds = abs(total_seconds)
+    minutes, seconds = divmod(total_seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    weeks, days = divmod(days, 7)
+    seconds = round(seconds)
+    minutes = math.floor(minutes)
+    hours = math.floor(hours)
+    days = math.floor(days)
+    weeks = math.floor(weeks)
+    result = ''
+    if (weeks > 0):
+        result += '{:d}w '.format(weeks)
+    result += '{:d}d {:d}h {:d}m {:d}s'.format(days, hours, minutes, seconds)
+    if include_relative_indicator:
+        if is_past:
+            result += ' ago'
         else:
-            return _get_item_ingredients_as_text(item_name, ingredients_dicts, item_design_data), True
+            result = 'in {}'.format(result)
+    return result
 
 
-def _get_item_ingredients_as_embed(item_name, ingredients_dicts, item_design_data):
-    return ''
+def get_utcnow():
+    return datetime.now(timezone.utc)
 
 
-def _get_item_ingredients_as_text(item_name, ingredients_dicts, item_design_data):
-    util.dbg_prnt(f'+ _get_item_ingredients_as_text({item_name}, {len(ingredients_dicts)} dicts, item_design_data)')
-    lines = [f'**Ingredients for {item_name}**']
-    lines.append('')
-    
-    dbg_i = 0
-    
-    for ingredients_dict in ingredients_dicts:
-        util.dbg_prnt(f'[_get_item_ingredients_as_text] Processing ingredients level {dbg_i}: {ingredients_dict}')
-        current_level_lines = []
-        dbg_ii = 0
-        for item_id, item_amount in ingredients_dict.items():
-            util.dbg_prnt(f'[_get_item_ingredients_as_text] Level {dbg_i}: Processing ingredient {dbg_ii}: ({item_id}: {item_amount})')
-            item_info = item_design_data[item_id]
-            util.dbg_prnt(f'[_get_item_ingredients_as_text] Level {dbg_i}, ingredient {dbg_ii}: Retrieved item info for item id: {item_id}')
-            item_name = item_info[ITEM_DESIGN_DESCRIPTION_PROPERTY_NAME]
-            util.dbg_prnt(f'[_get_item_ingredients_as_text] Level {dbg_i}, ingredient {dbg_ii}: Retrieved item name: {item_name}')
-            item_price = int(item_info['MarketPrice'])
-            util.dbg_prnt(f'[_get_item_ingredients_as_text] Level {dbg_i}, ingredient {dbg_ii}: Retrieved item price: {item_price}')
-            price_sum = item_price * item_amount
-            util.dbg_prnt(f'[_get_item_ingredients_as_text] Level {dbg_i}, ingredient {dbg_ii}: Calculated price_sum: {price_sum}')
-            current_level_lines.append(f'{item_amount} x {item_name} ({item_price} bux ea): {price_sum} bux')
-            dbg_ii += 1
-        lines.extend(current_level_lines)
-        lines.append('')
-        dbg_i += 1
-    
-    lines.append('Note: bux prices listed here may not always be accurate due to transfers between alts/friends or other reasons')
-    
-    util.dbg_prnt(f'+ _get_item_ingredients_as_text: returning {len(lines)} lines')
-    return lines
+async def get_latest_message(from_channel, by_member_id=None, with_content=None, after=None, before=None):
+    if from_channel is not None:
+        messages = from_channel.history(limit=100, after=after, before=before, older_first=True).flatten()
+        for msg in reversed(messages):
+            process = not by_member_id or msg.author.id == by_member_id
+            if process and msg.content == with_content:
+                return msg
+    return None
 
 
-def _parse_ingredients_tree(ingredients_str: str, item_design_data: dict, parent_amount: int = 1) -> list: # a nested list, basically a tree
-    """returns a tree structure: [(item_id, item_amount, item_ingredients[])]"""
-    if not ingredients_str:
-        return []
+def create_embed(title, description=None, colour=None, fields=None):
+    result = discord.Embed(title=title, description=description, colour=colour)
+    if fields is not None:
+        for t in fields:
+            result.add_field(name=t[0], value=t[1], inline=t[2])
+    return result
 
-    # Ingredients format is: [<id>x<amount>][|<id>x<amount>]*
-    ingredients_dict = dict([split_str.split('x') for split_str in ingredients_str.split('|')])
+
+def get_bot_member_colour(bot, guild):
+    bot_member = guild.get_member(bot.user.id)
+    bot_colour = bot_member.colour
+    return bot_colour
+
+
+def get_embed_field_def(title=None, text=None, inline=True):
+    return (title, text, inline)
+
+
+
+def dbg_prnt(text):
+    print(f'[{get_utcnow()}]: {text}')
+
+
+def create_posts_from_lines(lines, char_limit) -> list:
+    dbg_prnt(f'+ create_posts_from_lines({len(lines)} lines, {char_limit})')
     result = []
+    current_post = ''
     
     dbg_i = 0
-    for item_id, item_amount in ingredients_dict.items():
-        item_info = item_design_data[item_id]
-        item_name = item_info[ITEM_DESIGN_DESCRIPTION_PROPERTY_NAME].lower()
-        item_amount = int(item_amount)
-        # Filter out void particles and scrap
-        if 'void particle' not in item_name and ' fragment' not in item_name:
-            combined_amount = item_amount * parent_amount
-            item_ingredients = _parse_ingredients_tree(item_info['Ingredients'], item_design_data, combined_amount)
-            result.append((item_id, combined_amount, item_ingredients))
+    for line in lines:
+        dbg_prnt(f'[create_posts_from_lines] Processing line {dbg_i}')
+        line_length = len(line)
+        dbg_prnt(f'[create_posts_from_lines] Line {dbg_i} length: {line_length}')
+        dbg_prnt(f'[create_posts_from_lines] Current post length: {len(current_post)}')
+        dbg_prnt(f'[create_posts_from_lines] New post length: {len(current_post) + line_length + 1}')
+        if len(current_post) + line_length + 1 > char_limit:
+            dbg_prnt(f'[create_posts_from_lines] Appending current post to result.')
+            result.append(current_post)
+            dbg_prnt(f'[create_posts_from_lines] Clearing current post.')
+            current_post = ''
+        if len(current_post) > 0:
+            dbg_prnt(f'[create_posts_from_lines] Current post is not empty, adding line break.')
+            current_post += '\n'
+            
+        current_post += line
+        dbg_prnt(f'[create_posts_from_lines] Added line to current post: {line}')
         dbg_i += 1
-    # Result looks like this:
-    #   (item 1, amount 1,
-    #    - (item 1.1, amount 1.1 * amount 1, )
-    #      ...
-    #    - (item 1.n, amount 1.n * amount 1, 
-    #       - (item 1.n.1, amount 1.n.1 * amount 1.n * amount 1, )
-    #         ...
-    #       - (item 1.n.1, amount 1.n.1 * amount 1.n * amount 1, )))
-    #    item 2
-    #     - item 2.1
-    #     - item 2.n
+        
+    if current_post:
+        result.append(current_post)
     
+    dbg_prnt(f'- create_posts_from_lines: {len(result)} lines')
     return result
 
 
-def _flatten_ingredients_tree(ingredients_tree: list) -> list: # list of dicts
-    ingredients = {}
-    ingredients_without_subs = []
-    sub_ingredients = []
-    
-    for item_id, item_amount, item_ingredients in ingredients_tree:
-        # add all entries of current level to ingredients
-        if item_id in ingredients.keys():
-            ingredients[item_id] += item_amount
-        else:
-            ingredients[item_id] = item_amount
-        
-        if item_ingredients:
-            # add all entries of sublevel to sub_ingredients 
-            sub_ingredients.extend(item_ingredients)
-        else:
-            # add entries of current level without ingredients to ingredients_without_subs
-            ingredients_without_subs.append((item_id, item_amount, item_ingredients))
-    
-    result = [ingredients]
-    
-    if len(ingredients_without_subs) != len(ingredients_tree):
-        sub_ingredients.extend(ingredients_without_subs)
-        flattened_subs = _flatten_ingredients_tree(sub_ingredients)
-        result.extend(flattened_subs)
-    
+#---------- DB utilities ----------
+DB_TIMESTAMP_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+def db_get_column_definition(column_name, column_type, is_primary=False, not_null=False):
+    column_name_txt = column_name.upper()
+    column_type_txt = column_type.upper()
+    is_primary_txt = ''
+    not_null_txt = ''
+    if is_primary:
+        is_primary_txt = ' PRIMARY KEY'
+    if not_null:
+        not_null_txt = ' NOT NULL'
+    result = '{} {}{}{}'.format(column_name_txt, column_type_txt, is_primary_txt, not_null_txt)
+    return result
+
+
+def db_get_where_and_string(where_strings):
+    if where_strings:
+        result = ''
+        for i in range(0, len(where_strings)):
+            if i > 0:
+                result += ' AND '
+            result += where_strings[i]
+        return result
+
+
+def db_get_where_or_string(where_strings):
+    if where_strings:
+        result = ''
+        for i in range(0, len(where_strings)):
+            if i > 0:
+                result += ' OR '
+            result += where_strings[i]
+        return result
+
+
+def db_get_where_string(column_name, column_value, is_text_type=False):
+    column_name = column_name.lower()
+    if is_text_type:
+        column_value = db_convert_text(column_value)
+    return '{} = {}'.format(column_name, column_value)
+
+
+def db_convert_boolean(value):
+    if value:
+        return 'TRUE'
+    else:
+        return 'FALSE'
+
+def db_convert_text(value):
+    if value:
+        result = str(value)
+        result = result.replace('\'', '\'\'')
+        result = '\'{}\''.format(result)
+        return result
+    else:
+        return ''
+
+def db_convert_timestamp(datetime):
+    if datetime:
+        result = 'TIMESTAMPTZ \'{}\''.format(datetime.strftime(DB_TIMESTAMP_FORMAT))
+        return result
+    else:
+        return None
+
+def db_convert_to_boolean(db_boolean):
+    if db_boolean is None:
+        return None
+    db_upper = db_boolean.upper()
+    if db_upper == 'TRUE' or db_upper == '1' or db_upper == 'T' or db_upper == 'Y' or db_upper == 'YES':
+        return True
+    else:
+        return False
+
+def db_convert_to_datetime(db_timestamp):
+    if db_timestamp is None:
+        return None
+    result = db_timestamp.strptime(DB_TIMESTAMP_FORMAT)
+    return result
+
+def db_convert_to_int(db_int):
+    if db_int is None:
+        return None
+    result = int(db_int)
+    return result
+
+def db_convert_to_float(db_float):
+    if db_float is None:
+        return None
+    result = float(db_float)
     return result
