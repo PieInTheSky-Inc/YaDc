afe29f84e26ed937d6c8772493d89a8983937d82
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2019-09-19 10:07:57 +0200

    Added emojis

--- src/emojis.py
+++ src/emojis.py
@@ -2,3 +2,14 @@
 star = '⭐️'
 
 trophy = '🏆'
+
+pss_bux = '<:bux:582495505905352725>'
+pss_dove = '<:dove:615878722431483904>'
+pss_gas_big = '<:gas_big:586222567866499129>'
+pss_gas_small = '<:gas_small:586222568046985277>'
+pss_min_big = '<:min_big:586222661139693568>'
+pss_min_small = '<:min_small:586222672669835335>'
+pss_star_big = '<:star_big:615878809220022301>'
+pss_star_small = '<:star_small:615878662398410762>'
+pss_trophy_big = '<:trophy_big:615878873157992459>'
+pss_trophy_small = '<:trophy_small:615878860394856450>'
