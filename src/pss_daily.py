23ba3d50c6fb1e7e52831b7e1af95c93509a259a
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2019-11-21 21:55:44 +0100

    Refactoring

--- src/pss_daily.py
+++ src/pss_daily.py
@@ -3,25 +3,27 @@
 
 import os
 import pss_core as core
+import utility as util
 
-def try_store_daily_channel(guild_id, text_channel_id):
+
+def try_store_daily_channel(guild_id: int, text_channel_id: int) -> bool:
     success = False
     rows = select_daily_channel(guild_id, None)
     if len(rows) == 0:
         success = insert_daily_channel(guild_id, text_channel_id)
         if success == False:
-            print('[try_store_daily_channel] failed to insert new data row: {} ({})'.format(guild_id, text_channel_id))
+            print(f'[try_store_daily_channel] failed to insert new data row: {guild_id} ({text_channel_id})')
     else:
         if str(rows[0][1]) != str(text_channel_id):
             success = update_daily_channel(guild_id, text_channel_id, True)
             if success == False:
-                print('[try_store_daily_channel] failed to update data row: {} ({})'.format(guild_id, text_channel_id))
+                print(f'[try_store_daily_channel] failed to update data row: {guild_id} ({text_channel_id})')
         else:
             success = True
     return success
 
 
-def get_daily_channel_id(guild_id):
+def get_daily_channel_id(guild_id: int) -> int:
     rows = select_daily_channel(guild_id, None)
     if len(rows) == 0:
         return -1
@@ -30,7 +32,7 @@ def get_daily_channel_id(guild_id):
         return int(result)
 
 
-def get_all_daily_channel_ids():
+def get_all_daily_channel_ids() -> list:
     rows = select_daily_channel(None, None)
     if len(rows) == 0:
         return []
@@ -39,7 +41,7 @@ def get_all_daily_channel_ids():
         return results
 
 
-def get_valid_daily_channel_ids():
+def get_valid_daily_channel_ids() -> list:
     rows = select_daily_channel(None, True)
     if len(rows) == 0:
         return []
@@ -48,7 +50,7 @@ def get_valid_daily_channel_ids():
         return results
 
 
-def try_remove_daily_channel(guild_id):
+def try_remove_daily_channel(guild_id: int) -> bool:
     rows = select_daily_channel(guild_id)
     success = False
     if len(rows) == 0:
@@ -60,43 +62,37 @@ def try_remove_daily_channel(guild_id):
     return success
 
 
-def fix_daily_channel(guild_id, can_post):
-    success = update_daily_channel(guild_id, None, convert_can_post(can_post))
+def fix_daily_channel(guild_id: int, can_post: bool) -> bool:
+    success = update_daily_channel(guild_id, None, util.db_convert_boolean(can_post))
     return success
 
 
 # ---------- Utilities ----------
-def delete_daily_channel(guild_id):
+def delete_daily_channel(guild_id: int) -> bool:
     query = 'DELETE FROM serversettings WHERE guildid = \'{}\''.format(guild_id)
     success = core.db_try_execute(query)
     return success
 
-def insert_daily_channel(guild_id, channel_id):
-    query = 'INSERT INTO serversettings (guildid, channelid, canpost) VALUES ({},{},TRUE)'.format(guild_id, channel_id)
+def insert_daily_channel(guild_id: int, channel_id: int) -> bool:
+    query = 'INSERT INTO serversettings (guildid, dailychannelid, dailycanpost) VALUES ({},{},TRUE)'.format(guild_id, channel_id)
     success = core.db_try_execute(query)
     return success
 
-def select_daily_channel(guild_id = None, can_post = None):
+def select_daily_channel(guild_id: int = None, can_post: bool = None) -> list:
     query = 'SELECT * FROM serversettings'
     if guild_id:
         query += ' WHERE guildid = \'{}\''.format(guild_id)
         if can_post != None:
-            query += ' AND canpost = {}'.format(convert_can_post(can_post))
+            query += ' AND canpost = {}'.format(util.db_convert_boolean(can_post))
     if can_post != None:
-        query += ' WHERE canpost = {}'.format(convert_can_post(can_post))
+        query += ' WHERE canpost = {}'.format(util.db_convert_boolean(can_post))
     result = core.db_fetchall(query)
     return result
 
-def update_daily_channel(guild_id, channel_id = None, can_post = True):
+def update_daily_channel(guild_id: int, channel_id: int = None, can_post: bool = True) -> bool:
     query = 'UPDATE serversettings SET '
     if channel_id != None:
         query += 'channelid = \'{}\', '.format(channel_id)
-    query += 'canpost = {} WHERE guildid = \'{}\''.format(convert_can_post(can_post), guild_id)
+    query += 'dailycanpost = {} WHERE guildid = \'{}\''.format(util.db_convert_boolean(can_post), guild_id)
     success = core.db_try_execute(query)
     return success
-
-def convert_can_post(can_post):
-    if can_post:
-        return 'TRUE'
-    else:
-        return 'FALSE'
