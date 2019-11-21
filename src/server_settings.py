ab08b479e53e6c4d73dced6a00e5c36754b2a7fb
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2019-11-21 17:43:47 +0100

    Initial commit

---
+++ src/server_settings.py
@@ -0,0 +1,7 @@
+import pss_core as core
+
+def get_server_settings(guild_id) -> tuple:
+    # TODO: get server settings
+    pass
+
+# TODO: add functions for getting AND storing individual settings
