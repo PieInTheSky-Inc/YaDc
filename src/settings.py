30ff08fa8073981bde7cec211c29c3e11ba184ea
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2019-11-26 07:56:38 +0100

    Changed version

--- src/settings.py
+++ src/settings.py
@@ -25,6 +25,6 @@ SETTINGS_TYPES = ['boolean','float','int','text','timestamputc']
 
 USE_EMBEDS = False
 
-VERSION = '1.2.2.3'
+VERSION = '1.2.2.4'
 
 WIKIA_BASE_ADDRESS = 'https://pixelstarships.fandom.com/wiki/'
