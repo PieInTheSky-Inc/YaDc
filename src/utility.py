ccec36dfb2c9359e448c3f5a2a6195455a572af0
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2020-01-03 18:21:45 +0100

    Refactoring

--- src/utility.py
+++ src/utility.py
@@ -555,7 +555,7 @@ async def try_remove_reaction(reaction: discord.Reaction, user: discord.User) ->
         return False
 
 
-def get_exact_args(ctx: discord.ext.commands.Context) -> list:
+def get_exact_args(ctx: discord.ext.commands.Context) -> str:
     try:
         command = f'{ctx.prefix}{ctx.invoked_with} '
         args = str(ctx.message.content[len(command):])
