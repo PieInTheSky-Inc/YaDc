fdd444528f428f532acf27054d3378cddfcb19ea
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2020-04-08 12:37:42 +0200

    Fixed Issue #97

--- src/settings.py
+++ src/settings.py
@@ -33,7 +33,8 @@ GDRIVE_SERVICE_ACCOUNT_FILE = 'client_secrets.json'
 GDRIVE_SETTINGS_FILE = 'settings.yaml'
 GDRIVE_SCOPES = ['https://www.googleapis.com/auth/drive']
 
-LATEST_SETTINGS_BASE_URL = 'https://api.pixelstarships.com/SettingService/GetLatestVersion3?deviceType=DeviceTypeAndroid&languageKey='
+LATEST_SETTINGS_BASE_URL = 'https://api.pixelstarships.com/'
+LATEST_SETTINGS_BASE_PATH = 'SettingService/GetLatestVersion3?deviceType=DeviceTypeAndroid&languageKey='
 
 MAXIMUM_CHARACTERS = 1900
 MIN_ENTITY_NAME_LENGTH = 3
