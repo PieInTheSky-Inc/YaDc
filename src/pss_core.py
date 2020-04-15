f3bd099891160a9cb9ff5aef25b5a526df752d11
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2020-04-15 11:33:46 +0200

    Fixed bug

--- src/pss_core.py
+++ src/pss_core.py
@@ -345,7 +345,8 @@ async def get_latest_settings(language_key: str = 'en', use_base_production_serv
     if use_base_production_server:
         base_url = f'{settings.LATEST_SETTINGS_BASE_URL}{settings.LATEST_SETTINGS_BASE_PATH}'
     else:
-        base_url = f'{await get_base_url()}{settings.LATEST_SETTINGS_BASE_PATH}'
+        base_url = await get_base_url()
+        base_url = f'{base_url}{settings.LATEST_SETTINGS_BASE_PATH}'
     url = f'{base_url}{language_key}'
     raw_text = await get_data_from_url(url)
     result = xmltree_to_dict3(raw_text)
