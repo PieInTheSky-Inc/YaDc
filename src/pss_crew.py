fa0a144cdc80430f6c3fc488733e298f69e66f95
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2020-04-19 16:08:41 +0200

    Changed short text output for crew

--- src/pss_crew.py
+++ src/pss_crew.py
@@ -625,8 +625,8 @@ __properties: Dict[str, Union[entity.EntityDesignDetailProperty, List[entity.Ent
     ],
     'character_short': [
         entity.EntityDesignDetailProperty('Rarity', False, entity_property_name='Rarity'),
-        entity.EntityDesignDetailProperty('Ability', True, transform_function=__get_stat, stat_name='SpecialAbilityArgument'),
-        entity.EntityDesignDetailProperty('Collection', True, transform_function=__get_collection_name)
+        entity.EntityDesignDetailProperty('Ability', True, omit_if_none=True, transform_function=__get_stat, stat_name='SpecialAbilityArgument'),
+        entity.EntityDesignDetailProperty('Collection', True, omit_if_none=True, transform_function=__get_collection_name)
     ]
 }
 
