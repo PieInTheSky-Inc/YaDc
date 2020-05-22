be33c14e4b920920179b4406f29e87493fd740b7
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2020-05-22 16:08:12 +0200

    Added fixing datetime fields

--- src/excel.py
+++ src/excel.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 import openpyxl
 
 import pss_entity as entity
@@ -11,15 +11,16 @@ import utility as util
 # ---------- Constants ----------
 
 __BASE_TABLE_STYLE = openpyxl.worksheet.table.TableStyleInfo(name="TableStyleLight1", showFirstColumn=False, showLastColumn=False, showRowStripes=True, showColumnStripes=False)
+__EARLIEST_EXCEL_DATETIME = datetime.datetime(1900, 1, 1, tzinfo=datetime.timezone.utc)
 
 
-def create_xl_from_data(data: list, file_prefix: str, data_retrieval_date: datetime, column_formats: list, file_name: str = None) -> str:
-    if data_retrieval_date is None:
-        data_retrieval_date = util.get_utcnow()
+def create_xl_from_data(data: list, file_prefix: str, data_retrieved_at: datetime.datetime, column_formats: list, file_name: str = None) -> str:
+    if data_retrieved_at is None:
+        data_retrieved_at = util.get_utcnow()
     if file_name:
         save_to = file_name
     else:
-        save_to = get_file_name(file_prefix, data_retrieval_date)
+        save_to = get_file_name(file_prefix, data_retrieved_at)
 
     wb = openpyxl.Workbook()
     ws = wb.active
@@ -39,10 +40,10 @@ def create_xl_from_data(data: list, file_prefix: str, data_retrieval_date: datet
     return save_to
 
 
-def create_xl_from_raw_data_dict(flattened_data: list, entity_key_name: str, file_prefix: str, data_retrieval_date: datetime = None) -> str:
-    if data_retrieval_date is None:
-        data_retrieval_date = util.get_utcnow()
-    save_to = get_file_name(file_prefix, data_retrieval_date)
+def create_xl_from_raw_data_dict(flattened_data: list, entity_key_name: str, file_prefix: str, data_retrieved_at: datetime.datetime = None) -> str:
+    if data_retrieved_at is None:
+        data_retrieved_at = util.get_utcnow()
+    save_to = get_file_name(file_prefix, data_retrieved_at)
 
     header_names = []
     for row in flattened_data:
@@ -71,9 +72,9 @@ def create_xl_from_raw_data_dict(flattened_data: list, entity_key_name: str, fil
     return save_to
 
 
-def get_file_name(file_prefix: str, data_retrieval_date: datetime) -> str:
+def get_file_name(file_prefix: str, data_retrieved_at: datetime.datetime) -> str:
     file_prefix = file_prefix.replace(' ', '_')
-    file_timestamp = data_retrieval_date.strftime('%Y%m%d-%H%M%S')
+    file_timestamp = data_retrieved_at.strftime('%Y%m%d-%H%M%S')
     result = f'{file_prefix}_{file_timestamp}.xlsx'
     return result
 
@@ -91,12 +92,19 @@ def _convert_to_ref(column_count: int, row_count: int, column_start: int = 0, ro
 def _fix_field(field: str):
     if field:
         try:
-            return int(field)
+            dt = util.parse_pss_datetime(field)
+            if dt < __EARLIEST_EXCEL_DATETIME:
+                return __EARLIEST_EXCEL_DATETIME
+            else:
+                return dt
         except (TypeError, ValueError):
             try:
-                return float(field)
+                return int(field)
             except (TypeError, ValueError):
-                pass
+                try:
+                    return float(field)
+                except (TypeError, ValueError):
+                    pass
         field_lower = field.lower().strip()
         if field_lower == 'false':
             return False
