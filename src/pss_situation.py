3dbb09b6150b3c3ee4f7b643ed5790f88451cdde
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2020-12-30 21:54:25 +0100

    Changed output, fixed bugs & refactoring

--- src/pss_situation.py
+++ src/pss_situation.py
@@ -51,7 +51,7 @@ SITUATION_TRIGGER_TYPE_LOOKUP: Dict[str, str] = {
 
 # ---------- Event info ----------
 
-def get_event_details_by_id(situation_design_id: str, situations_data: EntitiesData) -> entity.entity.EntityDetails:
+def get_event_details_by_id(situation_design_id: str, situations_data: EntitiesData) -> entity.EntityDetails:
     if situation_design_id:
         if situation_design_id and situation_design_id in situations_data.keys():
             return __create_situation_details_from_info(situations_data[situation_design_id], situations_data)
@@ -64,12 +64,12 @@ async def get_event_details(ctx: Context, situation_id: str = None, all_events: 
         raise ValueError(f'Only one of these parameters may be True: situation_id, all_events, latest_only')
     utc_now = utils.get_utc_now()
     situations_data = await situations_designs_retriever.get_data_dict3()
-    situation_infos = sorted(situations_data.values(), key=lambda x: (utils.parse.pss_datetime(x['EndDate']), -int(x[SITUATION_DESIGN_KEY_NAME])), reverse=True)
+    situation_infos = sorted(situations_data.values(), key=lambda x: (utils.parse.pss_datetime(x['EndDate']), int(x[SITUATION_DESIGN_KEY_NAME])), reverse=True)
 
     if situation_id:
         situation_infos = [situations_data[situation_id]]
     elif all_events:
-        situation_infos = list(situations_data.values())
+        situation_infos.reverse()
     elif latest_only:
         situation_infos = [situation_infos[0]]
     else:
@@ -81,14 +81,25 @@ async def get_event_details(ctx: Context, situation_id: str = None, all_events: 
         else:
             raise NotFound(f'There\'s no event running currently.')
     else:
-        situations_details_collection = __create_situations_details_collection_from_infos(situation_infos, situations_data, utc_now=utc_now)
+        chars_data = await crew.characters_designs_retriever.get_data_dict3()
+        collections_data = await crew.collections_designs_retriever.get_data_dict3()
+        items_data = await item.items_designs_retriever.get_data_dict3()
+        missions_data = await mission.missions_designs_retriever.get_data_dict3()
+        rooms_data = await room.rooms_designs_retriever.get_data_dict3()
+
+        situations_details_collection = __create_situations_details_collection_from_infos(situation_infos, situations_data, chars_data, collections_data, items_data, missions_data, rooms_data, utc_now=utc_now)
         if as_embed:
             return (await situations_details_collection.get_entities_details_as_embed(ctx))
         else:
             return (await situations_details_collection.get_entities_details_as_text())
 
 
-async def __get_event_reward(change_type: str, change_argument: str, for_embed: Optional[bool] = False) -> Optional[str]:
+async def get_current_events_details(situations_data: EntitiesData, utc_now: datetime) -> List[entity.EntityDetails]:
+    result = __get_current_situation_infos(situations_data, utc_now)
+    return result
+
+
+async def __get_event_reward(change_type: str, change_argument: str, chars_data: EntitiesData, collections_data: EntitiesData, items_data: EntitiesData, for_embed: Optional[bool] = False) -> Optional[str]:
     if for_embed is None:
         for_embed = False
     result = None
@@ -97,14 +108,11 @@ async def __get_event_reward(change_type: str, change_argument: str, for_embed: 
     if entity.entity_property_has_value(change_argument):
         entity_type, entity_id, reward_amount, _ = utils.parse.entity_string(change_argument, default_amount=None)
         if entity_type == 'character':
-            characters_data = await crew.characters_designs_retriever.get_data_dict3()
-            collections_data = await crew.collections_designs_retriever.get_data_dict3()
-            reward_details = crew.get_char_details_by_id(entity_id, characters_data, collections_data)
+            reward_details = crew.get_char_details_by_id(entity_id, chars_data, collections_data)
         elif entity_type == 'item':
-            items_data = await item.items_designs_retriever.get_data_dict3()
             reward_details = item.get_item_details_by_id(entity_id, items_data, None)
         else:
-            reward_amount = change_argument
+            reward_amount = int(change_argument)
     if reward_details:
         details_text = "".join(await reward_details.get_details_as_text(entity.EntityDetailsType.MINI, for_embed=for_embed))
         if reward_amount:
@@ -112,7 +120,7 @@ async def __get_event_reward(change_type: str, change_argument: str, for_embed: 
         else:
             result = details_text
     elif change_type == 'AddLeagueBonusGas':
-        result = f'+{reward_amount} % {emojis.pss_gas_big} from league bonus'
+        result = f'{reward_amount+100} % league bonus for {emojis.pss_gas_big}'
     elif change_type == 'AddEXP':
         result = f'{reward_amount} % exp from battles'
     else:
@@ -125,16 +133,16 @@ async def __get_event_reward(change_type: str, change_argument: str, for_embed: 
 
 # ---------- Transformation functions ----------
 
-async def __get_event_type_and_reward(situation_info: EntityInfo, situations_data: EntitiesData, **kwargs) -> Optional[str]:
+async def __get_event_type_and_reward(situation_info: EntityInfo, situations_data: EntitiesData, chars_data: EntitiesData, collections_data: EntitiesData, items_data: EntitiesData, missions_data: EntitiesData, rooms_data: EntitiesData, **kwargs) -> Optional[str]:
     for_embed = kwargs.get('for_embed', False)
     change_argument = situation_info.get('ChangeArgumentString')
     change_type = situation_info.get('ChangeType')
-    reward = await __get_event_reward(change_type, change_argument, for_embed=for_embed)
+    reward = await __get_event_reward(change_type, change_argument, chars_data, collections_data, items_data, for_embed=for_embed)
     result = f'{SITUATION_CHANGE_TYPE_LOOKUP.get(change_type)}: {reward}'
     return result
 
 
-def __get_situation_chance(situation_info: EntityInfo, situations_data: EntitiesData, **kwargs) -> Optional[str]:
+def __get_situation_chance(situation_info: EntityInfo, situations_data: EntitiesData, chars_data: EntitiesData, collections_data: EntitiesData, items_data: EntitiesData, missions_data: EntitiesData, rooms_data: EntitiesData, **kwargs) -> Optional[str]:
     chance = situation_info.get('Chance')
     if entity.entity_property_has_value(chance):
         result = f'{chance} %'
@@ -143,7 +151,7 @@ def __get_situation_chance(situation_info: EntityInfo, situations_data: Entities
     return result
 
 
-async def __get_situation_requirements(situation_info: EntityInfo, situations_data: EntitiesData, **kwargs) -> Optional[str]:
+async def __get_situation_requirements(situation_info: EntityInfo, situations_data: EntitiesData, chars_data: EntitiesData, collections_data: EntitiesData, items_data: EntitiesData, missions_data: EntitiesData, rooms_data: EntitiesData, **kwargs) -> Optional[str]:
     for_embed = kwargs.get('for_embed', False)
     requirement_str = situation_info.get('RequirementString')
     requirements = utils.parse.requirement_string(requirement_str)
@@ -158,11 +166,9 @@ async def __get_situation_requirements(situation_info: EntityInfo, situations_da
             details = None
             entity_details = None
             if entity_type == 'mission':
-                missions_data = await mission.missions_designs_retriever.get_data_dict3()
                 entity_info = missions_data[entity_id]
                 details = f'Mission "{entity_info[mission.MISSION_DESIGN_DESCRIPTION_PROPERTY_NAME]}" completed'
             if entity_type == 'room':
-                rooms_data = await room.rooms_designs_retriever.get_data_dict3()
                 entity_details = room.get_room_details_by_id(entity_id, rooms_data, None, None, None)
             elif entity_type == 'shiplevel':
                 resource_key = f'AMOUNTMODIFIER{entity_amount_modifier}'
@@ -176,7 +182,7 @@ async def __get_situation_requirements(situation_info: EntityInfo, situations_da
     return result
 
 
-def __get_situation_trigger(situation_info: EntityInfo, situations_data: EntitiesData, **kwargs) -> Optional[str]:
+def __get_situation_trigger(situation_info: EntityInfo, situations_data: EntitiesData, chars_data: EntitiesData, collections_data: EntitiesData, items_data: EntitiesData, missions_data: EntitiesData, rooms_data: EntitiesData, **kwargs) -> Optional[str]:
     trigger_type = situation_info.get('TriggerType')
     if entity.entity_property_has_value(trigger_type):
         return SITUATION_TRIGGER_TYPE_LOOKUP.get(trigger_type, trigger_type)
@@ -188,12 +194,12 @@ def __get_situation_trigger(situation_info: EntityInfo, situations_data: Entitie
 
 # ---------- Create entity.entity.EntityDetails ----------
 
-def __create_situation_details_from_info(situation_info: EntityInfo, situations_data: EntitiesData, utc_now: datetime = None) -> entity.entity.EntityDetails:
-    return entity.entity.EntityDetails(situation_info, __properties['title'], __properties['description'], __properties['properties'], __properties['embed_settings'], situations_data, utc_now=utc_now)
+def __create_situation_details_from_info(situation_info: EntityInfo, situations_data: EntitiesData, chars_data: EntitiesData, collections_data: EntitiesData, items_data: EntitiesData, missions_data: EntitiesData, rooms_data: EntitiesData, utc_now: datetime = None) -> entity.entity.EntityDetails:
+    return entity.entity.EntityDetails(situation_info, __properties['title'], __properties['description'], __properties['properties'], __properties['embed_settings'], situations_data, chars_data, collections_data, items_data, missions_data, rooms_data, utc_now=utc_now)
 
 
-def __create_situations_details_collection_from_infos(situations_designs_infos: List[EntityInfo], situations_data: EntitiesData, utc_now: datetime = None) -> entity.EntityDetailsCollection:
-    situations_details = [__create_situation_details_from_info(situation_info, situations_data, utc_now=utc_now) for situation_info in situations_designs_infos]
+def __create_situations_details_collection_from_infos(situations_designs_infos: List[EntityInfo], situations_data: EntitiesData, chars_data: EntitiesData, collections_data: EntitiesData, items_data: EntitiesData, missions_data: EntitiesData, rooms_data: EntitiesData, utc_now: datetime = None) -> entity.EntityDetailsCollection:
+    situations_details = [__create_situation_details_from_info(situation_info, situations_data, chars_data, collections_data, items_data, missions_data, rooms_data, utc_now=utc_now) for situation_info in situations_designs_infos]
     result = entity.EntityDetailsCollection(situations_details, big_set_threshold=0)
     return result
 
@@ -212,7 +218,10 @@ def __get_current_situation_infos(situations_infos: List[EntityInfo], utc_now: d
             start_at = utils.parse.pss_datetime(from_date)
             end_at = utils.parse.pss_datetime(end_date)
             if start_at <= utc_now and end_at > utc_now:
+                situation_info['from_date'] = from_date
+                situation_info['end_date'] = end_date
                 result.append(situation_info)
+    result = sorted(result, key=lambda x: (x['end_date'], x['from_date'], int(x[SITUATION_DESIGN_KEY_NAME])), reverse=True)
     return result
 
 
@@ -247,6 +256,9 @@ __properties: entity.EntityDetailsCreationPropertiesCollection = {
             entity.EntityDetailProperty('Requirement', True, entity_property_name='RequirementString', transform_function=__get_situation_requirements, text_only=True),
             entity.EntityDetailProperty('Starts at', True, entity_property_name='FromDate', transform_function=core.transform_pss_datetime_with_timespan, display_inline_for_embeds=False, omit_time_if_zero=True, include_seconds_in_timespan=False),
             entity.EntityDetailProperty('Ends at', True, entity_property_name='EndDate', transform_function=core.transform_pss_datetime_with_timespan, display_inline_for_embeds=False, omit_time_if_zero=True, include_seconds_in_timespan=False),
+        ],
+        properties_short=[
+            entity.EntityDetailProperty('Ends at', True, entity_property_name='EndDate', transform_function=core.transform_pss_datetime_with_timespan, display_inline_for_embeds=False, omit_time_if_zero=True, include_seconds_in_timespan=False)
         ],
         properties_mini=[]
         ),
