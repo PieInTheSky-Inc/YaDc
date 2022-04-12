e6ed844b54a6074db2c3587d8c42ae9532136143
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2022-04-12 19:59:17 +0200

    Fixed bug in `wiki data crafts`

--- src/pss_craft.py
+++ src/pss_craft.py
@@ -4,7 +4,7 @@ from typehints import EntitiesData, EntityInfo
 
 # ---------- Constants ----------
 
-CRAFT_DESIGN_BASE_PATH: str = 'RoomService/ListCraftDesigns2?languageKey=en'
+CRAFT_DESIGN_BASE_PATH: str = 'RoomService/ListCraftDesigns?languageKey=en'
 CRAFT_DESIGN_DESCRIPTION_PROPERTY_NAME: str = 'CraftName'
 CRAFT_DESIGN_KEY_NAME: str = 'CraftDesignId'
 
