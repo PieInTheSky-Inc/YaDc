4d1f2eab4305d87d99fa9689b2fd36224a994089
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2022-08-17 14:55:58 +0200

    Converted /item

--- src/cogs/current.py
+++ src/cogs/current.py
@@ -1407,6 +1407,21 @@ class CurrentDataSlashCog(_CogBase, name='Current PSS Data Slash'):
         await _utils.discord.respond_with_output(ctx, output)
 
 
+    @_slash_command(name='item', brief='Get item stats')
+    @_cooldown(rate=_CogBase.RATE, per=_CogBase.COOLDOWN, type=_BucketType.user)
+    async def item_slash(self,
+        ctx: _Context,
+        item_name: _Option(str, 'Enter the item name')
+    ):
+        """
+        Get the stats of any item matching the given item name.
+        """
+        self._log_command_use(ctx)
+
+        output = await _item.get_item_details_by_name(ctx, item_name, as_embed=(await _server_settings.get_use_embeds(ctx)))
+        await _utils.discord.respond_with_output(ctx, output)
+
+
     @_slash_command(name='upgrade', brief='Get crafting recipes')
     @_cooldown(rate=_CogBase.RATE, per=_CogBase.COOLDOWN, type=_BucketType.user)
     async def upgrade_slash(self,
