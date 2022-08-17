fe841810bc6822a0088a4fc95a4416593587c74e
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2022-08-17 23:01:10 +0200

    Converted /training

--- src/cogs/current.py
+++ src/cogs/current.py
@@ -1667,6 +1667,22 @@ class CurrentDataSlashCog(_CogBase, name='Current PSS Data Slash'):
         await _utils.discord.respond_with_output(ctx, output)
 
 
+    @_slash_command(name='training', brief='Get training infos')
+    @_cooldown(rate=_CogBase.RATE, per=_CogBase.COOLDOWN, type=_BucketType.user)
+    async def training_slash(self,
+        ctx: _ApplicationContext,
+        training_name: _Option(str, description='Enter training name.')
+    ):
+        """
+        Get detailed information on one or more training(s).
+        """
+        self._log_command_use(ctx)
+
+        await ctx.interaction.response.defer()
+        output = await _training.get_training_details_from_name(training_name, ctx, as_embed=(await _server_settings.get_use_embeds(ctx)))
+        await _utils.discord.respond_with_output(ctx, output)
+
+
     @_slash_command(name='upgrade', brief='Get crafting recipes')
     @_cooldown(rate=_CogBase.RATE, per=_CogBase.COOLDOWN, type=_BucketType.user)
     async def upgrade_slash(self,
