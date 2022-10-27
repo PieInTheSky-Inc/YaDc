77ea3aae08d9ddb75cf9e80248469aa670b57829
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2022-10-27 20:21:50 +0200

    Updated version

--- src/settings.py
+++ src/settings.py
@@ -98,7 +98,7 @@ USE_EMBEDS: bool = True
 USE_ACCESS_TOKEN: int = int(os.environ.get('USE_ACCESS_TOKEN', 0))
 
 
-VERSION: str = '1.3.10.13'
+VERSION: str = '1.3.11.0b'
 
 
 WIKI_COMMAND_GUILDS: List[str] = json.loads(os.environ.get('WIKI_COMMAND_GUILDS', '[]'))
